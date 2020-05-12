30576e075b1e508773e07787d47424199487dc20
Kyle Smith <kwsmith@sdsu.edu>
2020-05-11 17:56:38 -0700

    add layer

--- super-basemap-test.py
+++ super-basemap-test.py
@@ -2,7 +2,16 @@ import pandas as pd
 import folium 
 import geopandas
 
+buildings = 'https://raw.githubusercontent.com/sdsu-fis/super-basemap-test/master/buildings.geojson'
 
 m = folium.Map(location=[32.77460, -117.07195], zoom_start=15.4)
+
+folium.GeoJson(
+    buildings,
+    name='geojson'
+).add_to(m)
+
+folium.LayerControl().add_to(m)
+
 
 m.save('index.html')
